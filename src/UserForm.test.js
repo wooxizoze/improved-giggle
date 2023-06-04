28d6aea0fb8332c2e1df33db395c24dd1277a767
dmitrykrasnov-orca <dmitryk@orca.security>
2023-06-04 22:43:48 +0300

    Add test for onUserAdd

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react'
-import {user} from '@testing-library/user-event'
+import user from '@testing-library/user-event'
 import UserForm from './UserForm'
 
 test('it shows two inputs and a button', () => { 
@@ -12,4 +12,24 @@ test('it shows two inputs and a button', () => {
     // make an assertion
     expect(inputs).toHaveLength(2)
     expect(button).toBeInTheDocument()
+})
+
+test('it calls onUserAdd when form is submited', () => {
+    // render component
+    const mock = jest.fn()
+    render(<UserForm onUserAdd={mock}/>)
+    // find two inputs 
+    const [nameInput, emailInput] = screen.getAllByRole('textbox')
+    // simulate type a name 
+    user.click(nameInput)
+    user.keyboard('walter')
+    // simulate type an email 
+    user.click(emailInput)
+    user.keyboard('walter.white@gmail.com')
+    // simulate clicking a form 
+    const button = screen.getByRole('button')
+    user.click(button)
+    // assertion that onUserAdd
+    expect(mock).toHaveBeenCalled()
+    expect(mock).toHaveBeenCalledWith({name:'walter', email:'walter.white@gmail.com'})
 })
